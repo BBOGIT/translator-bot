adafb5c3123515eaae69d736d63450febfa7ce9d
Volodymyr Kornieiev <Vol.kor.93@gmail.com>
2023-05-24 20:42:28 +0300

    fix 1

--- src/config/datebase.ts
+++ src/config/datebase.ts
@@ -1,11 +1,9 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('words-db', '', '', {
-    host: './dev.sqlite',  
-    dialect: 'sqlite',
-    logging: false
-})
+const db = new Sequelize("words-db", "", "", {
+  host: "./dev.sqlite",
+  dialect: "sqlite",
+  logging: false,
+});
 
 export default db;
-
-/Users/bohdanbuhriienko/Documents/Lessons/itkamas/src/config/datebase.ts
--- src/routes/words-router.ts
+++ src/routes/words-router.ts
@@ -1,82 +1,108 @@
-import { Router, Request, Response } from "express"
-import db from '../config/datebase'
-import WordInstance from "../model/index"
-import {v4 as uuidv4} from 'uuid'
-import WordValidator from "../validator/index"
-import Middleware from "../middleware/index"
+import { Router, Request, Response } from "express";
+import db from "../config/datebase";
+import WordInstance from "../model/index";
+import { v4 as uuidv4 } from "uuid";
+import WordValidator from "../validator/index";
+import Middleware from "../middleware/index";
 
-db.sync().then(() => console.log('db is ready'))
+db.sync().then(() => console.log("db is ready"));
 
-export const wordsRouter = Router({})
+export const wordsRouter = Router({});
 
-wordsRouter.get('/', WordValidator.checkReadWord(), Middleware.checkValidationResult,   
-async (req: Request, res: Response) => {
-    console.log(req.body)
+wordsRouter.get(
+  "/",
+  WordValidator.checkReadWord(),
+  Middleware.checkValidationResult,
+  async (
+    // Типизировать параметрі реквеста нужно тут
+    req: Request & { query: { limit?: number; offset?: number } },
+    res: Response
+  ) => {
+    console.log(req.body);
     try {
-        const limit = req.query?.limit as number | undefined;
-        const offset = req.query?.limit as number | undefined;
+      // А тут нужно указать параметры по умолчанию, если вдруг параметрі не прилетели,
+      // если они выше валидируються то типы можно указать соответственно
+      const limit = req.query?.limit || 10;
+      const offset = req.query?.limit || 0;
 
-        const data = await WordInstance.findAll({where: {}, limit, offset});
-        return res.status(200).json({data})
-    } catch (error) {       
-        return res.status(500).json({message: 'Error while getting records'})
-    }
-})
-
-wordsRouter.get('/:id', WordValidator.checkIdParam(), Middleware.checkValidationResult,
-async (req: Request, res: Response) => {
-    console.log(req.body)
-    try {
-        const data = await WordInstance.findOne({where: {id: req.params.id}});
-        return res.status(200).json({data})
+      const data = await WordInstance.findAll({ where: {}, limit, offset });
+      return res.status(200).json({ data });
     } catch (error) {
-        return res.status(500).json({message: 'Error while getting record'})
+      return res.status(500).json({ message: "Error while getting records" });
     }
-})
+  }
+);
 
-wordsRouter.post('/', WordValidator.checkCreateWord(), Middleware.checkValidationResult, 
-async (req: Request, res: Response) => {
+wordsRouter.get(
+  "/:id",
+  WordValidator.checkIdParam(),
+  Middleware.checkValidationResult,
+  async (req: Request, res: Response) => {
+    console.log(req.body);
+    try {
+      const data = await WordInstance.findOne({ where: { id: req.params.id } });
+      return res.status(200).json({ data });
+    } catch (error) {
+      return res.status(500).json({ message: "Error while getting record" });
+    }
+  }
+);
+
+wordsRouter.post(
+  "/",
+  WordValidator.checkCreateWord(),
+  Middleware.checkValidationResult,
+  async (req: Request, res: Response) => {
     const id = uuidv4();
-try {
-    const data = await WordInstance.create({...req.body, id});
-    return res.status(201).json({data, message: 'Successfully created'})
-} catch (error) {
-    return res.status(500).json({message: 'Error while creating record'})
-}
-})
-
-wordsRouter.put('/:id', WordValidator.checkIdParam(), Middleware.checkValidationResult,
- async (req: Request, res: Response) => {
-    console.log(req.body)
     try {
-        const data = await WordInstance.findOne({where: {id: req.params.id}});
-        
-        if (!data) {
-            return res.status(404).json({message: 'Record not found'})
-        }
-
-        const updatedRecord = await data.update({
-            ...req.body
-        });
-        return res.status(200).json({data: updatedRecord})
+      const data = await WordInstance.create({ ...req.body, id });
+      return res.status(201).json({ data, message: "Successfully created" });
     } catch (error) {
-        return res.status(500).json({message: 'Error while updating record'})
+      return res.status(500).json({ message: "Error while creating record" });
     }
-})
+  }
+);
 
-wordsRouter.delete('/:id', WordValidator.checkIdParam(), Middleware.checkValidationResult,
- async (req: Request, res: Response) => {
-    console.log(req.body)
+wordsRouter.put(
+  "/:id",
+  WordValidator.checkIdParam(),
+  Middleware.checkValidationResult,
+  async (req: Request, res: Response) => {
+    console.log(req.body);
     try {
-        const data = await WordInstance.findOne({where: {id: req.params.id}});
-        
-        if (!data) {
-            return res.status(404).json({message: 'Record not found'})
-        }
+      const data = await WordInstance.findOne({ where: { id: req.params.id } });
 
-        await data.destroy();
-        return res.status(204)
+      if (!data) {
+        return res.status(404).json({ message: "Record not found" });
+      }
+
+      const updatedRecord = await data.update({
+        ...req.body,
+      });
+      return res.status(200).json({ data: updatedRecord });
     } catch (error) {
-        return res.status(500).json({message: 'Error while updating record'})
+      return res.status(500).json({ message: "Error while updating record" });
     }
-})
+  }
+);
+
+wordsRouter.delete(
+  "/:id",
+  WordValidator.checkIdParam(),
+  Middleware.checkValidationResult,
+  async (req: Request, res: Response) => {
+    console.log(req.body);
+    try {
+      const data = await WordInstance.findOne({ where: { id: req.params.id } });
+
+      if (!data) {
+        return res.status(404).json({ message: "Record not found" });
+      }
+
+      await data.destroy();
+      return res.status(204);
+    } catch (error) {
+      return res.status(500).json({ message: "Error while updating record" });
+    }
+  }
+);
